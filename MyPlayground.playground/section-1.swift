5dc586b282790ac211ecded666295c2dccf5d88b
Leila Chan Currie <l.chancurrie@gmail.com>
2014-10-08 23:28:36 -0400

    Initial Commit

---
+++ MyPlayground.playground/section-1.swift
@@ -0,0 +1,26 @@
+class TipCalculator{
+    let total: Double
+    let taxPct: Double
+    let subtotal: Double
+
+    init(total:Double, taxPct:Double) {
+        self.total = total
+        self.taxPct = taxPct
+        subtotal = total / (taxPct + 1)
+    }
+
+    func calcTipWithTipPct(tipPct:Double) -> Double {
+        return subtotal * tipPct
+    }
+
+    func printPossibleTips() {
+        println("15%: \(calcTipWithTipPct(0.15))")
+        println("18%: \(calcTipWithTipPct(0.18))")
+        println("20%: \(calcTipWithTipPct(0.20))")
+    }
+
+
+}
+
+let tipCalc = TipCalculator(total: 33.25, taxPct: 0.06)
+tipCalc.printPossibleTips()
